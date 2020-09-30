39e7703ed4af544f8971aaddb26553cb80798843
Alex <alexjimenezes@gmail.com>
2020-09-30 21:30:00 +0200

    Semana 1 hecha, necesita testint exhaustivo

---
+++ Datos.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+# coding: utf-8
+import pandas as pd
+import numpy as np
+
+
+class Datos:
+
+    # TODO: procesar el fichero para asignar correctamente las variables nominalAtributos, datos y diccionarios
+    def __init__(self, nombreFichero):
+
+        # Read file with pandas
+        file = pd.read_csv(nombreFichero)
+
+        self.nominalAtributos = np.ones(len(file.dtypes), dtype=bool)
+        # If the type of the column is a string / object the value in the array is True
+        # If it is a float or integer it will be False
+        # Otherwise we raise an exception
+        for i in range(len(file.dtypes)):
+            if file.dtypes[i] == object:
+                pass
+            elif file.dtypes[i] == np.int64 or file.dtypes[i] == np.float64:
+                self.nominalAtributos[i] = False
+            else:
+                raise ValueError('Columns must be either nominal values, floats or integers')
+
+        tmp_datos = file.to_numpy()
+        # It will be a dict of dicts to store all the possible categorical variables from different fields
+        # We implement it this so in case of collision of names, the weights will not be affected.
+        self.diccionario = dict()
+
+        # We iterate column per column
+        contador = 0
+        for j in range(tmp_datos.shape[1]):
+            # If the column is nominal we get a set of its objects
+            if self.nominalAtributos[j]:
+                self.diccionario[file.columns[j]] = dict()
+                unique = sorted(set(tmp_datos[:, j]))
+                # Add the ordered set of words to the corresponding dictionary
+                for u in unique:
+                    if u not in self.diccionario:
+                        self.diccionario[file.columns[j]][u] = contador
+                        contador += 1
+
+        # Replace dictionary values of nominal variables in datos
+        self.datos = np.empty(tmp_datos.shape)
+        for j in range(tmp_datos.shape[1]):
+            # If not nominal the just copy the entire column
+            if not self.nominalAtributos[j]:
+                self.datos[:, j] = tmp_datos[:, j]
+            # If nominal, go one by one extracting the value from the dictionary
+            else:
+                for i in range(tmp_datos.shape[0]):
+                    self.datos[i, j] = self.diccionario[file.columns[j]][tmp_datos[i][j]]
+
+    # This method will give back a matrix composed of the index passed by argument
+    def extraeDatos(self, idx):
+        matrix = np.empty((len(idx), self.datos.shape[1]))
+        for id in range(len(idx)):
+            matrix[id] = self.datos[idx[id]]
+        return matrix
---
+++ EstrategiaParticionado.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+# coding: utf-8
+
+from abc import ABCMeta,abstractmethod
+import random
+
+class Particion():
+
+  # Esta clase mantiene la lista de �ndices de Train y Test para cada partici�n del conjunto de particiones  
+  def __init__(self):
+    self.indicesTrain=[]
+    self.indicesTest=[]
+
+#####################################################################################################
+
+class EstrategiaParticionado(object, metaclass=ABCMeta):
+  
+  # Atributos: deben rellenarse adecuadamente para cada estrategia concreta. Se pasan en el constructor
+  @abstractmethod
+  def __init__(self):
+    self.particiones = list()
+  
+  @abstractmethod
+  # TODO: esta funcion deben ser implementadas en cada estrategia concreta  
+  def creaParticiones(self,datos,seed=None):
+    pass
+  
+
+#####################################################################################################
+
+class ValidacionSimple(EstrategiaParticionado):
+  
+  def __init__(self, proporcionTest, numeroEjecuciones):
+    super().__init__()
+    self.proporcionTest = proporcionTest
+    self.numeroEjecuciones = numeroEjecuciones
+
+  # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el n�mero de ejecuciones deseado
+  # Devuelve una lista de particiones (clase Particion)
+  def creaParticiones(self,datos,seed=None):
+    random.seed(seed)
+
+    # Get how many rows will be used for test & train
+    idxForTest = int(self.proporcionTest * datos.shape[0])
+
+    # Create same number of partitions as number of iterations exist
+    for i in range(self.numeroEjecuciones):
+      idx = list(range(datos.shape[0]))
+      random.shuffle(idx)
+      p = Particion()
+      p.indicesTest = idx[:idxForTest]
+      p.indicesTrain = idx[idxForTest:]
+      self.particiones.append(p)
+
+    return self.particiones
+      
+#####################################################################################################      
+class ValidacionCruzada(EstrategiaParticionado):
+
+  def __init__(self, numeroParticiones):
+    super().__init__()
+    self.numeroParticiones = numeroParticiones
+
+  # Crea particiones segun el metodo de validacion cruzada.
+  # El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
+  # Esta funcion devuelve una lista de particiones (clase Particion)
+  # TODO: implementar
+  def creaParticiones(self,datos,seed=None):   
+    random.seed(seed)
+    resto = datos.shape[0] % self.numeroParticiones
+    rows = int(datos.shape[0] / self.numeroParticiones)
+    last_index = 0
+    for i in range(self.numeroParticiones):
+      numero_rows = rows
+      if resto > 0:
+        numero_rows += 1
+        resto -= 1
+      p = Particion()
+      p.indicesTest = list(range(last_index, last_index + numero_rows))
+      p.indicesTrain = list(range(last_index)) + list(range(last_index + numero_rows, datos.shape[0])) 
+      last_index += numero_rows
+      self.particiones.append(p)
+  
+    return self.particiones
+
+
+
+
+ 
+    
